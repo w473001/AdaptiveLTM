e078c09fa3786aaf6cc86c035865ea04dac09f25
w473001 <106175285+w473001@users.noreply.github.com>
2022-05-24 17:43:54 +0100

    second commit

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from src.backtest.parallel_backtest import run_parallel_backtest
 
 
 def main(from_config):
-    run_parallel_backtest(from_config=config_name, base_seed=12345)
+    run_parallel_backtest(from_config=from_config, base_seed=12345)
 
 
 if __name__ == '__main__':
-    config_name = 'tuned_config_multi_after'
+    config_name = 'tuned_config_multi'
     main(config_name)
